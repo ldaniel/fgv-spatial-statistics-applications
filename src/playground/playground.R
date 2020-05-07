f227463fd98ee505b2e9a182b026b084b0424910
ldaniel <contato@leandrodaniel.com>
2020-05-07 09:48:04 -0300

    Add initial solution template based on FGVR library

---
+++ src/playground/playground.R
@@ -0,0 +1,62 @@
+# loading required libraries ----------------------------------------------------------
+# FGVR library for data science power-ups
+library(fgvr)
+
+# libraries for data prep
+library(dplyr)
+library(readr)
+library(magrittr)
+library(forcats)
+library(lubridate)
+library(stringr)
+library(feather)
+library(fastDummies)
+library(reshape2)
+library(knitr)
+
+#libraries for plots
+library(ggplot2)
+library(ggthemes)
+library(ggcorrplot)
+library(ggpubr)
+library(plotly)
+
+# libraries for data clean
+library(VIM)
+library(rms)
+library(mctest)
+
+# libraries for modeling
+library(caret)
+library(gmodels)
+library(MASS)
+library(rpart)
+library(rpart.plot)
+library(adabag)
+library(randomForest)
+
+# libraries for spatial data manipulation
+library(spatialreg)
+library(maps)
+library(maptools)    
+library(rgdal)     
+library(sp)  
+library(spdep)
+library(bamlss)
+library(gstat)
+library(splancs)
+library(spatstat)
+library(pgirmess)
+library(classInt)
+library(spgwr)
+
+# libraries for measures
+library(hmeasure)
+library(pROC)
+
+# loading other scripts do be used here ---------------------------------------
+source("./src/util/auxiliary_functions.R")
+
+# well, here is your playground. 
+# feel free to do whatever you want to find out 
+# the best approach for you data science project. =)
