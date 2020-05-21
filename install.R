9ebd606e55b9de9e3667967e567e42c91f73f6a2
RodriGonca <rodrigo.goncalves@me.com>
2020-05-21 14:43:10 -0300

    adding suport for biinder

---
+++ install.R
@@ -0,0 +1,24 @@
+install.packages('Quandl')
+
+# data prep libraries
+install.packages('tibble')
+install.packages('magrittr')
+install.packages('dplyr')
+install.packages('plyr')
+install.packages('tidyr')
+install.packages('readr')
+install.packages('readxl')
+install.packages('stringr')
+install.packages('lubridate')
+install.packages('janitor')
+install.packages('ggmap')
+install.packages('rgdal')
+
+# plotting libraries
+install.packages('ggplot2')
+install.packages('ggthemes')
+install.packages('ggpubr')
+
+# knit libraries
+install.packages('knitr')
+install.packages('rmarkdown')
--- src/playground/playground.R
+++ src/playground/playground.R
@@ -9,56 +9,22 @@ library(magrittr)
 library(forcats)
 library(lubridate)
 library(stringr)
-library(feather)
-library(fastDummies)
-library(reshape2)
 library(knitr)
 
 #libraries for plots
 library(ggplot2)
 library(ggthemes)
-library(ggcorrplot)
 library(ggpubr)
-library(plotly)
 
-# libraries for data clean
-library(VIM)
-library(rms)
-library(mctest)
-
-# libraries for modeling
-library(caret)
-library(gmodels)
-library(MASS)
-library(rpart)
-library(rpart.plot)
-library(adabag)
-library(randomForest)
-
-# libraries for spatial data manipulation
-library(spatialreg)
-library(maps)
-library(maptools)    
+#libraries for spatial statistics
 library(rgdal)     
-library(sp)  
-library(spdep)
-library(bamlss)
-library(gstat)
-library(splancs)
-library(spatstat)
-library(pgirmess)
-library(classInt)
-library(spgwr)
-
-# libraries for measures
-library(hmeasure)
-library(pROC)
 
 # create CSV files to be read by Power BI -------------------------------------
 # write.csv(gas_prices_hist,    file = "data/processed/gas_prices_hist.csv")
 # write.csv(gas_prices_station, file = "data/processed/gas_prices_station.csv")
 
 # first regression atempt ----
+
 model <- lm(data = gas_prices_station, 
             formula = 'PcVenda ~ DistMean + DistDev + DistMin + DistMax + RefinMean + RefinDev + RefinMin  + RefinMax')
 
