1ef73d2dfc37ce562b340f4b1751f8b47e530553
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2020-06-11 09:19:19 -0300

    Creating a thematic map with PcMedRev column and gas station

---
+++ src/playground/mapa_tematico_pcmedrev.R
@@ -0,0 +1,74 @@
+################################################################################################
+#
+# APLICAÇÕES EM ESTATÍSTICA ESPACIAL - MBA Business Analytics e Big Data
+#
+# CASE - MAPA TEMÁTICO DE MUNICÍPIOS E POSTOS
+#
+################################################################################################
+
+
+# PACOTES -----------------------------------------------------------------
+
+
+# carrega as extensões SF, TMAP, DPLYR e PRYR
+library(tmap)
+library(sf)
+library(dplyr)
+library(pryr)
+
+
+# CARREGA MAPAS, DADOS E PREPARA O MAPA BASE ------------------------------
+
+#Carrega o shapefile do mapa utilizando o pacote SF (Mais simples que o RGDAL)
+mapa <- st_read("C:/Users/Ricardo/Documents/R-Projetos/Spatial-Statistics-Applications/data/processed/gas_prices_hist/gas_prices_hist.shp")
+
+#O pacote SF carrega o shapefile como um dataframe (Diferente do RGDAL que carrega um SpatialPolygonsDataFrame)
+class(mapa)
+
+#Plota apenas os polígonos do mapa
+plot(st_geometry(mapa))
+
+
+# CRIANDO O DF DE LOCALIZAçÕES E TRANSFORMANDO EM SF ----------------------
+
+gas_prices_station <- read.csv("~/R-Projetos/Spatial-Statistics-Applications/data/processed/gas_prices_station.csv")
+
+#Pegar o CRS do mapa
+st_crs(mapa)
+
+#Transforma o DF de pontos em um formato para mapas adicionando o mesmo CRS do mapa
+#Pode-se usar também  "crs = 4326" quando as coordenadas forem lat/long
+localidades <- st_as_sf(gas_prices_station, coords = c("Lon", "Lat"), crs = "+proj=longlat +datum=WGS84 +no_defs")
+
+#Plota os pontos para testar
+plot(st_geometry(localidades))
+
+
+# RENDERIZA O MAPA --------------------------------------------------------
+
+#Seta o mapa para o formato interativo (Com leaflet)
+tmap_mode("view")
+
+#Seta o título do gráfico
+titulo = "Preço Médio de Revenda (R$)"
+
+#Seta o os valores das quebras na legenda
+breaks = c(3, 3.5, 4, 4.5, 5) 
+
+#Primeira layer = Municípios
+tm_shape(mapa) +
+  
+  #Utiliza as cores para montar o mapa temático baseado na coluna PcMedRev
+  tm_fill(col = "PcMedRev", title = titulo, alpha = 0.8, palette = c("blue","white","red"), breaks = breaks) +
+  
+  #Insere bordas para facilitar a visualização das áreas
+  tm_borders() +
+  
+  #Adiciona o estilo cobalt
+  tm_style("cobalt") +
+  
+  #Insere a segunda layer = Localidades (lat,lon)
+  tm_shape(localidades) + 
+  
+  #Desenha os pontos
+  tm_dots(size = 0.05, col = "green", alpha =0.6)
